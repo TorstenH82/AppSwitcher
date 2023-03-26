37377931c35903a7c8676539fb95bb37f22fcc66
TorstenH82 <to.hoffmeister@gmail.com>
2023-03-26 20:54:36 +0200

    2.7

--- app/src/main/java/com/thf/appswitcher/utils/RunActivity.java
+++ app/src/main/java/com/thf/appswitcher/utils/RunActivity.java
@@ -11,35 +11,41 @@ import java.util.Iterator;
 import java.util.List;
 
 public class RunActivity implements Runnable {
-    private static final String TAG = "AppSwitcherService";
+  private static final String TAG = "AppSwitcherService";
 
-    private String packageName;
-    private String activity;
-    private Context context;
+  private String packageName;
+  private String activity;
+  private Context context;
 
-    public RunActivity(Context context, String packageName, String activity) {
-        this.context = context;
-        this.packageName = packageName;
-        this.activity = activity;
+  public RunActivity(Context context, String packageName, String activity) {
+    this.context = context;
+    this.packageName = packageName;
+    this.activity = activity;
+  }
+
+  @Override
+  public void run() {
+    if (!Utils.isPackageInstalled(context, packageName)) {
+      Log.d(TAG, "package " + packageName + " is not installed");
+      return;
     }
 
-    @Override
-    public void run() {
-        if (!Utils.isPackageInstalled(context, packageName)){
-            Log.d(TAG, "package " + packageName + " is not installed");
-            return;
-        }
-        
-        
-        Log.d(TAG, "run activity " + packageName + "/" + activity);
-        ComponentName name = new ComponentName(packageName, activity);
-        Intent intentStartMedia = new Intent(Intent.ACTION_MAIN);
-        //intentStartMedia.addCategory(Intent.CATEGORY_LAUNCHER);
-        intentStartMedia.setFlags(
-                Intent.FLAG_ACTIVITY_NEW_TASK
-                        | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
-                        | Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        intentStartMedia.setComponent(name);
-        context.startActivity(intentStartMedia);
-    }
+    Log.d(TAG, "run activity " + packageName + "/" + activity);
+    ComponentName name = new ComponentName(packageName, activity);
+    Intent intent = new Intent(Intent.ACTION_MAIN);
+    // intent.addCategory(Intent.CATEGORY_LAUNCHER);
+    intent.setFlags(
+        Intent.FLAG_ACTIVITY_NEW_TASK
+            | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
+            | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+    intent.setComponent(name);
+    context.startActivity(intent);
+
+    //switch to home and start activity again    
+    Intent startMain = new Intent(Intent.ACTION_MAIN);
+    startMain.addCategory(Intent.CATEGORY_HOME);
+    startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+    context.startActivity(startMain);
+    context.startActivity(intent);
+  }
 }
